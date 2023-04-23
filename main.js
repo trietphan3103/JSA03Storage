3dd7b765dfb58d742b7d4d679d310a975e5dc9d0
trietphan3103 <minhtriet143@gmail.com>
2023-04-23 14:15:29 +0700

    deploy theband template

---
+++ main.js
@@ -0,0 +1,9 @@
+let school = "MindX School"
+let name = "Phan Minh Triết"
+let job = "giảng viên"
+
+// let tên biến = giá trị => cú pháp
+
+let result = "Tôi tên là " + name + ". Tôi đang là " + job + " tại " + school;
+console.log(result) 
+// Tôi tên là Phan Minh Triết. Tôi đang là giảng viên tại MindX School
